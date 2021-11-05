b95061af6ff8e181bbccdec841b1c08a149c607e
TalisMan701 <boss.kompaneec@mail.ru>
2021-11-05 23:59:55 +0500

    Verstka intro section

--- src/Components/Buttons/ButtonMain/ButtonMain.jsx
+++ src/Components/Buttons/ButtonMain/ButtonMain.jsx
@@ -53,6 +53,56 @@ const ButtonMain = (props) => {
 			</div>
 		)
 	}
+	if(props.type === "gray"){
+		return (
+			<div
+				className={clsx(classes.inner ,classes.innerGray)}
+				onClick={props.onClick}
+				style={{
+					...props.style,
+					borderStyle: "solid",
+					width: props.width - props.borderWidth * 2,
+					height: props.height - props.borderWidth * 2,
+					borderWidth: props.borderWidth,
+					borderRadius: props.borderRadius
+				}}
+			>
+				<div
+					className={clsx(classes.text, classes.textGray)}
+					style={{
+						fontSize: props.fontSize,
+					}}
+				>
+					{props.label}
+				</div>
+			</div>
+		)
+	}
+	if(props.type === "red"){
+		return (
+			<div
+				className={clsx(classes.inner ,classes.innerRed)}
+				onClick={props.onClick}
+				style={{
+					...props.style,
+					borderStyle: "solid",
+					width: props.width - props.borderWidth * 2,
+					height: props.height - props.borderWidth * 2,
+					borderWidth: props.borderWidth,
+					borderRadius: props.borderRadius
+				}}
+			>
+				<div
+					className={clsx(classes.text, classes.textRed)}
+					style={{
+						fontSize: props.fontSize,
+					}}
+				>
+					{props.label}
+				</div>
+			</div>
+		)
+	}
 	return (
 		<div
 			className={classes.inner}
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import classes from "../StylesForPages/Landing.module.css"
 import LogoSvg from "../Images/SVG/Logo.svg"
+import EmtyImg from "../Images/Other/emtyGray.jpg"
+import CalendarSvg from "../Images/SVG/Calendar.svg"
+import EyeSvg from "../Images/SVG/Eye.svg"
 import clsx from "clsx";
 import {Link} from "react-router-dom";
 import ButtonMain from "../Components/Buttons/ButtonMain/ButtonMain";
@@ -41,6 +44,63 @@ const Landing = () => {
 					</div>
 				</div>
 			</header>
+			<main>
+				<section className={classes.intro}>
+					<div className={clsx(classes.container, classes.introInner)}>
+						<div className={classes.introCol1}>
+							<div className={classes.introTitle}>
+								Заработок в режиме автопилота
+							</div>
+							<div className={classes.introDesc}>
+								Первая обучающая платформа, на которой мы делимся, а ты приумножаешь
+							</div>
+							<ButtonMain
+								onClick={()=>{}}
+								label={"Начать"}
+								type={"red"}
+								fontSize={32}
+								borderWidth={2}
+								borderRadius={8}
+								width={268}
+								height={60}
+							/>
+						</div>
+						<div className={classes.introCol2}>
+							<img src={EmtyImg} alt="intro" className={classes.introImg}/>
+							<div className={clsx(classes.introCard, classes.introCard1)}>
+								<img src={CalendarSvg} alt="introCard" className={classes.introCardImg}/>
+								<div className={classes.introCardText}>Доход от 50 тыс. рублей в первый месяц</div>
+								<ButtonMain
+									style={{minWidth: 66}}
+									onClick={()=>{}}
+									label={"Try"}
+									type={"gray"}
+									fontSize={14}
+									borderWidth={2}
+									borderRadius={8}
+									width={66}
+									height={46}
+								/>
+							</div>
+							<div className={clsx(classes.introCard, classes.introCard2)}>
+								<img src={EyeSvg} alt="introCard" className={classes.introCardImg}/>
+								<div className={classes.introCardText}>Стартовый капитал - 990 рублей</div>
+								<ButtonMain
+									style={{minWidth: 66}}
+									onClick={()=>{}}
+									label={"Try"}
+									type={"gray"}
+									fontSize={14}
+									borderWidth={2}
+									borderRadius={8}
+									width={66}
+									height={46}
+								/>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
 		</>
 	);
 };
